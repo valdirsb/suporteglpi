712a53b4c5835997e920568c68935e438933248b
valdirsb <valdirsb@gmail.com>
2020-03-19 22:50:26 -0300

    Config

--- src/screes/Config.js
+++ src/screes/Config.js
@@ -7,8 +7,10 @@ import Axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
 import RNFetchBlob from 'react-native-fetch-blob';
 
+/*
 window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest;
 window.Blob = RNFetchBlob.polyfill.Blob;
+*/
 
 const Config = () => {
     const [ avatar, setAvatar ] = useState({});
@@ -61,8 +63,8 @@ const Config = () => {
                     input:
                         {
                             "type": 1,
-                            "name": "Ticket Teste 2",
-                            "content": "content': 'Content do ticket Teste 2"
+                            "name": "Ticket fetch",
+                            "content": "Content do ticket Teste 2"
                         }
                 },
           redirect: 'follow'
@@ -94,6 +96,41 @@ const Config = () => {
 
     async function uploadImage2() {
 
+
+        const token = await AsyncStorage.getItem('@token');
+
+        console.log(foto);
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "multipart/form-data");
+        myHeaders.append("Session-Token", token);
+        
+        const arquivoName = avatar.fileName;
+        const arquivoUri = avatar
+
+        var raw = JSON.stringify({
+                    input:{name:"Teste 1234",content:"Texto content"}
+                });
+
+        var formdata = new FormData();
+        formdata.append("uploadManifest", raw);
+        formdata.append("file", arquivoUri , arquivoName);
+        
+
+        
+
+        fetch('http://grupofst.com.br/hb7ti/apirest.php/Ticket', {
+            method: 'POST',
+            headers: myHeaders,
+            body: formdata,
+            redirect: 'follow'
+        })
+        .then((response) => response.json())
+        .then((result) => console.log(result))
+        .catch(error => console.log('error', error));
+
+        /*
+
         const imagesData = new FormData();
 
         
@@ -122,6 +159,8 @@ const Config = () => {
             console.log(imagesData);
             alert(err);
         }
+
+        */
 
         
 
