d7363aa9490fdfb0dbe0f31b27796f7aad2064ff
valdirsb <valdirsb@gmail.com>
2020-03-31 17:49:54 -0300

    Exclusao de arquivos antigos

--- srcNav/functions/functions.js
+++
@@ -1,4 +0,0 @@
-export default ir = (r) => {
-    props.navigation.navigate(r);
-}
-
--- srcNav/index.js
+++
@@ -1,4 +0,0 @@
-import { createAppContainer } from 'react-navigation';
-import MainNavigator from './navigators/MainNavigator';
-
-export default createAppContainer(MainNavigator);
